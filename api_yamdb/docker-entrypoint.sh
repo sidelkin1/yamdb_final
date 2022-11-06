359686c530127847af037046fb38cec2b958dfca
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2022-11-06 21:58:54 +0400

    Fixed entrypoint

--- api_yamdb/docker-entrypoint.sh
+++ api_yamdb/docker-entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# python manage.py migrate
-# python manage.py loaddb
-# python manage.py createsuperuser2 --noinput
-# python manage.py collectstatic --no-input
+apt-get update
+apt-get install postgresql-client -y
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+python manage.py migrate
+python manage.py loaddb
+python manage.py createsuperuser2 --noinput
+python manage.py collectstatic --no-input
 gunicorn api_yamdb.wsgi:application --bind 0:8000
 
 exec "$@"
