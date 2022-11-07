678160ae3b2530d4efee866a6449efde82e4a5d0
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2022-11-07 18:03:28 +0400

    Minor fixes

--- api_yamdb/docker-entrypoint.sh
+++ api_yamdb/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-apt-get update
-apt-get install postgresql-client -y
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
